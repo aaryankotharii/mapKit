12dfc83cf8ee0389490113668a57608cd9726f58
aaryankotharii <aaryan.kothari@gmail.com>
2020-02-06 01:24:26 +0530

    added address

--- Maps/liveMapViewController.swift
+++ Maps/liveMapViewController.swift
@@ -10,19 +10,80 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController {
+class liveMapViewController: UIViewController {
 
+    
     @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        checkLocationServicesEnables()
     }
+    
+    func setupLocationManager(){
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    
+    func checkLocationServicesEnables(){
+        if CLLocationManager.locationServicesEnabled() {
+            setupLocationManager()
+            checkLocationAuthorization()
+        }
+        else{
+            let alert = UIAlertController(title: "Error", message: "Please Turn on Locations from settings", preferredStyle: .alert)
+            let action = UIAlertAction(title: "ok", style: .default, handler: nil)
+            alert.addAction(action)
+        }
+    }
+    
+    func checkLocationAuthorization(){
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+        mapView.showsUserLocation = true
+        centreViewOnUsersLocation()
+        locationManager.startUpdatingLocation()
+        break
+        case .denied:
+        break
+        case .notDetermined:
+        locationManager.requestWhenInUseAuthorization()
+        break
+        case .restricted:
+        break
+        case .authorizedAlways:
+        break
+        @unknown default:
+            fatalError()
+        }
+    }
+    
+    func centreViewOnUsersLocation(){
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: 10000, longitudinalMeters: 1000)
+            mapView.setRegion(region, animated: true)
+        }
+        
+    }
+    
 
 
 }
 
 
-extension MapScreen : CLLocationManagerDelegate {
-    didupdate
+extension liveMapViewController : CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        let centre = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion(center: centre, latitudinalMeters: 5000, longitudinalMeters: 5000)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkLocationAuthorization()
+    }
 }
 
